a4ed69add2cbe61ab69e906c158dc3a5d5bcc69d
someshdev <43148672+someshdev@users.noreply.github.com>
2018-12-09 21:11:39 +0100

    added test case

---
+++ AST_Project/TestCase.py
@@ -0,0 +1,30 @@
+from Robot_recognizer import Robot
+import numpy as np
+import unittest
+import Robot_recognizer
+class KnownValues(unittest.TestCase):
+    def test_confidence_level_1(self):
+        reading1=np.array([("knife",1, 0.99), ("scissor", 2, 0.65), ("spoon", 3, 0.33), ("spoon", 4, 0.80), ("keys", 5, 0.95)])
+        reading2=np.array([ ("keys", 5, 0.95), ("spoon", 4, 0.99),("fork", 3, 0.99), ("scissor", 2, 0.95), ("knife",1, 0.55)])
+        expected =[('knife', 1, 0.99), ('scissor', 2, 0.95), ('fork', 3, 0.99), ('spoon', 4, 0.99), ('keys', 5, 0.95)]
+        Result_obj=Robot()
+        Result_obj.input(reading1,reading2)
+        result =Result_obj.recognise()
+        self.assertEqual(expected,result)
+    def test_confidence_level_2(self):
+        expected =None
+        Result_obj=Robot()
+        Result_obj.input()
+        result =Result_obj.recognise()
+        self.assertEqual(expected,result)    
+    def test_confidence_level_3(self):
+        reading1=np.array([('knife',1, 0.99), ('scissor', 2, 0.65), ('spoon', 3, 0.33)])
+        reading2=None
+        expected =[('knife',1, 0.99), ('scissor', 2, 0.65), ('spoon', 3, 0.33)]
+        Result_obj=Robot()
+        Result_obj.input(reading1)
+        result =Result_obj.recognise()
+        self.assertEqual(expected,result) 
+
+if __name__=='__main__':
+    unittest.main()
